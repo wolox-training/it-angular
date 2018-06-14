b67b4a3e0a7f736a8b2ad290211cef6212f05d68
Ivan <tammaroivan@gmail.com>
2018-06-14 12:55:30 -0300

    Register Layout

--- wolox-books/src/app/app.component.ts
+++ wolox-books/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app';
+  form: FormGroup;
+  post: any[];
+  firstName: string = '';
+  lastName: string = '';
+  email: string = '';
+  password: string = '';
+  locale: string = 'en';
+
+  constructor(private fb: FormBuilder){
+    this.form = fb.group({
+      'firstName': [null, Validators.required],
+      'lastName': [null, Validators.required],
+      'email': [null, Validators.required],
+      'password': [null, Validators.required]
+    })
+  }
+
+  register(post) {
+    post['locale'] = this.locale;
+    console.log(JSON.stringify({'user':post}));
+  }
 }
