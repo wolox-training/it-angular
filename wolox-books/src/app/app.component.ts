ed1cadc50ec74aee8eab49b5063e783182cd6425
Ivan <tammaroivan@gmail.com>
2018-06-15 14:09:52 -0300

    .scss global files

--- wolox-books/src/app/app.component.ts
+++ wolox-books/src/app/app.component.ts
@@ -9,16 +9,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 export class AppComponent {
   form: FormGroup;
-  firstName: string = '';
-  lastName: string = '';
+  first_name: string = '';
+  last_name: string = '';
   email: string = '';
   password: string = '';
   locale: string = 'en';
 
   constructor(private fb: FormBuilder){
     this.form = fb.group({
-      'firstName': [null, Validators.required],
-      'lastName': [null, Validators.required],
+      'first_name': [null, Validators.required],
+      'last_name': [null, Validators.required],
       'email': [null, Validators.required],
       'password': [null, Validators.required]
     })
