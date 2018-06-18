9b2434b27747a70ee25380b7cafa87175fb0f696
Ivan <tammaroivan@gmail.com>
2018-06-18 08:49:19 -0300

    Book list uncomplete

---
+++ wolox-books/src/app/services/token-interceptor.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor } from '@angular/common/http';
+import { LocalStorageService } from './local-storage.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenInterceptorService implements HttpInterceptor {
+
+  constructor(private storage: LocalStorageService) { }
+
+  intercept(req, next){
+    let tokenizedRequest = req.clone({
+      setHeaders: {
+        Authorization: 'Bearer ' +  this.storage.getValue(this.storage.SESSION_TOKEN)
+      }
+    });
+    return next.handle(tokenizedRequest);
+  }
+}
