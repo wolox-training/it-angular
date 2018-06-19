71a45c624a561a9b56b429fbc8a93caec665a7c2
Ivan <tammaroivan@gmail.com>
2018-06-19 09:24:43 -0300

    Some fixes

--- wolox-books/src/app/screens/unauth/screens/signup/sign-up.component.ts
+++ wolox-books/src/app/screens/unauth/screens/signup/sign-up.component.ts
@@ -13,7 +13,10 @@ export class SignUpComponent {
   form: FormGroup;
   locale: string = 'en';
 
-  constructor(private fb: FormBuilder, private UserService: UserService){
+  constructor(
+    private fb: FormBuilder,
+    private UserService: UserService
+  ){
     this.form = fb.group({
       'first_name': [null, Validators.required],
       'last_name': [null, Validators.required],
@@ -22,8 +25,7 @@ export class SignUpComponent {
     })
   }
 
-  register(post) {
-    post['locale'] = this.locale;
-    this.UserService.createUser({'user':{...post, 'password_confirmation': post.password}});
+  register() {
+    this.UserService.createUser({'user':{...this.form.value, 'password_confirmation': this.form.value.password, 'locale': this.locale}});
   }
 }
